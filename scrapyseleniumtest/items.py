bbae7b94d8df7094288e87b88af0596649761a0e
youdi <liangchangyoujackson@gmail.com>
2017-08-20 20:20:50 +0800

    init

---
+++ scrapyseleniumtest/items.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+
+# class ScrapyseleniumtestItem(scrapy.Item):
+#     # define the fields for your item here like:
+#     # name = scrapy.Field()
+#     pass
+
+
+class ProductItem(Item):
+    collection = 'products'
+    image = Field()
+    price = Field()
+    deal = Field()
+    title = Field()
+    shop = Field()
+    location = Field()
